fcaa38219ac12f9fb051fc3ef6ef7746eabc9c96
OmkarG <omkargample@gmail.com>
2024-10-27 13:38:04 +0530

    New Code

--- class.c
+++ class.c
@@ -1,12 +1,42 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#define MAX 50
 int main()
 {
-    printf("Hello world");
+    int N,arr[MAX],i,j,freq[MAX];
 
-    printf("New Branch");
+    printf("Enter number of elements:\t");
+    scanf("%d",&N);
 
-    printf("Pull request");
+    for(i=0;i<N;i++)
+    {
+        printf("Arr[%d]: ",i);
+        scanf("%d",&arr[i]);
+        freq[i] = 1;
+    }
+
+
+    for(i=0;i<N;i++)
+    {
+        if(freq[i]==0)
+           continue;
+        for(j=i+1;j<N;j++)
+        {
+           if(freq[j]==0)
+            continue;
+
+            if(arr[i]==arr[j])
+            {
+                freq[i]++;
+                freq[j]=0;
+            }
+        }
+    }
+    for(i=0;i<N;i++)
+    {
+        if(freq[i]!=0)
+           printf("Freq of %d is this times %d\n",arr[i],freq[i]);
+    }
+
 
     return 0;
 }
